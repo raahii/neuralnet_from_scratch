fix batch normalization

lib/activation_functions.py
@@ -73,7 +73,11 @@ class BatchNormalization:
         self.dgamma = None
 
     def forward(self, x, train_flg=True):
-        self.x = x
+        if x.ndim != 2:
+            self.x = x.reshape(x.shape[0], -1)
+        else:
+            self.x = x
+
         self.batch_size = x.shape[0]
         self.input_dim = x.shape[1]
 
@@ -91,16 +95,17 @@ class BatchNormalization:
 
     def backward(self, dy):
         self.dbeta = np.sum(dy)
-        self.dgamma = np.sum(self.x * dy, axis = 0)
+        self.dgamma = np.sum(self.x_norm * dy, axis = 0)
 
         dx_norm = dy * self.gamma
 
         dxc = dx_norm / self.std
-        dstd = -np.sum((dx_norm * self.xc) / (self.std * self.std), axis=0)
+        dstd = -np.sum(dx_norm * self.xc / (self.std * self.std), axis=0)
         dvar = 0.5 * dstd / self.std
-        dxc += (2.0 / self.batch_size) * self.xc * dvar
+        dxc += 2.0 / self.batch_size * self.xc * dvar
         dmu = np.sum(dxc, axis=0)
         dx = dxc - dmu / self.batch_size
+        # dx = dxc * ( 1.0 - 1.0 / self.batch_size)
 
         # TODO: should refactor design
         self.gamma -= self.lr * self.dgamma
@@ -139,7 +144,7 @@ class Pooling:
 
         Y = np.max(X, axis = 1)
         y = Y.reshape(BS, OH, OW, C).transpose(0, 3, 1, 2)
-        
+
         return y
 
     def backward(self, dy):

lib/layers.py
@@ -18,6 +18,8 @@ class Affine:
         self.x  = None
         self.activation_functions = np.array([activation_function]).flatten()
 
+        self.org_shape = None
+
     def init_params(self, method_name):
         coeffs = {
                 "gaussian": 0.01,
@@ -29,7 +31,9 @@ class Affine:
                np.random.randn(self.input_size, self.output_size)
 
     def forward(self, x, train_flg=True):
-        self.x = x
+        self.org_shape = x.shape
+        self.x = x.reshape(x.shape[0], -1)
+
         y = np.dot(self.x, self.W) + self.b
         
         for af in self.activation_functions:
@@ -38,9 +42,6 @@ class Affine:
         return y
 
     def backward(self, dy):
-        org_shape = x.shape
-        self.x = to_ndim(x, 2)
-
         for af in reversed(self.activation_functions):
             dy = af.backward(dy)
 
@@ -49,7 +50,7 @@ class Affine:
 
         dx = np.dot(dy, self.W.T)
 
-        return y.reshape(org_shape)
+        return dx
 
     def set_params(self, W, b):
         self.W = W