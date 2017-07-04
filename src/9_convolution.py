aa378518ee8af7c49955c782f2b4758a0e33682a
piyo56 <piyo56.net@gmail.com>
2017-07-04 19:42:05 +0900

    add depth

--- src/9_convolution.py
+++ src/9_convolution.py
@@ -4,16 +4,20 @@ import sys
 import numpy as np
 
 def conv(img, kernel, bias = 0, padding = 0, stride = 1):
+    if img.ndim == 2:
+        img = np.array([img])
+    if kernel.ndim == 2:
+        kernel = np.array([kernel])
+
     p = padding
     s = stride
-    h, w   = img.shape
-    fh, fw = kernel.shape
+    d, h, w = img.shape
+    fd, fh, fw = kernel.shape
     nh = int((h + 2*p - fh) / s) + 1
     nw = int((w + 2*p - fw) / s) + 1
 
-    if kernel.ndim != 1:
-        kernel = kernel.reshape(-1)
-
+    kernel = kernel.reshape(d, -1)
+    
     if padding != 0:
         _h, _w = h, w
         h, w = h+2*p, w+2*p
@@ -22,12 +26,12 @@ def conv(img, kernel, bias = 0, padding = 0, stride = 1):
         img = new_img
 
     c = np.zeros((nh, nw))
-    
     for i in range(0, nh):
         for j in range(0, nw):
-            t = img[s*i:s*i+fh, s*j:s*j+fw].reshape(-1)
-            # import pdb; pdb.set_trace()
-            c[i, j] = np.dot(t, kernel)
+            for k in range(0, d):
+                t = img[k, s*i:s*i+fh, s*j:s*j+fw].reshape(-1)
+                # import pdb; pdb.set_trace()
+                c[i, j] += np.dot(kernel, t)
 
     return c + bias
 
