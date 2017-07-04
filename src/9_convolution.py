853eac040fc552e350d2bb4cd494f38885cf9e63
piyo56 <piyo56.net@gmail.com>
2017-07-04 20:02:20 +0900

    add fn(filter num)

--- src/9_convolution.py
+++ src/9_convolution.py
@@ -4,36 +4,37 @@ import sys
 import numpy as np
 
 def conv(img, kernel, bias = 0, padding = 0, stride = 1):
+    # change ndim
     if img.ndim == 2:
-        img = np.array([img])
+        img = img.reshape(((1, 1)+img.shape)[-3:])
     if kernel.ndim == 2:
-        kernel = np.array([kernel])
+        kernel = kernel.reshape(((1, 1)+kernel.shape)[-4:])
 
     p = padding
     s = stride
-    d, h, w = img.shape
-    fd, fh, fw = kernel.shape
-    nh = int((h + 2*p - fh) / s) + 1
-    nw = int((w + 2*p - fw) / s) + 1
+    rd, rh, rw = img.shape
+    fn, fd, fh, fw = kernel.shape
+    nh = int((rh + 2*p - fh) / s) + 1
+    nw = int((rw + 2*p - fw) / s) + 1
 
-    kernel = kernel.reshape(d, -1)
+    kernel = kernel.reshape(fn, rd, -1)
     
     if padding != 0:
-        _h, _w = h, w
-        h, w = h+2*p, w+2*p
-        new_img = np.zeros((h, w))
+        _h, _w = rh, rw
+        rh, rw = rh+2*rp, rw+2*rp
+        new_img = np.zeros((rh, rw))
         new_img[p:p+_h, p:p+_w] = img
         img = new_img
 
-    c = np.zeros((nh, nw))
-    for i in range(0, nh):
-        for j in range(0, nw):
-            for k in range(0, d):
-                t = img[k, s*i:s*i+fh, s*j:s*j+fw].reshape(-1)
-                # import pdb; pdb.set_trace()
-                c[i, j] += np.dot(kernel, t)
+    ret = np.zeros((fn, nh, nw))
+    for i in range(nh):
+        for j in range(nw):
+            for c in range(fn):
+                for d in range(fd):
+                    t = img[d, s*i:s*i+fh, s*j:s*j+fw].reshape(-1)
+                    ret[c, i, j] += np.dot(kernel[c, d], t)
 
-    return c + bias
+    return ret + bias
 
 # img = np.array([
 #     [1, 2, 3, 0],
