3fed276bfbcd435ce3998cba6baa3822830eda5a
piyo56 <piyo56.net@gmail.com>
2017-07-04 20:26:15 +0900

    add pooling

--- src/9_convolution.py
+++ src/9_convolution.py
@@ -5,10 +5,8 @@ import numpy as np
 
 def conv(img, kernel, bias = 0, padding = 0, stride = 1):
     # change ndim
-    if img.ndim == 2:
-        img = img.reshape(((1, 1)+img.shape)[-4:])
-    if kernel.ndim == 2:
-        kernel = kernel.reshape(((1, 1)+kernel.shape)[-4:])
+    img = img.reshape(((1, 1)+img.shape)[-4:])
+    kernel = kernel.reshape(((1, 1)+kernel.shape)[-4:])
 
     p = padding
     s = stride
@@ -36,26 +34,55 @@ def conv(img, kernel, bias = 0, padding = 0, stride = 1):
 
     return ret + bias
 
-# img = np.array([
-#     [1, 2, 3, 0],
-#     [0, 1, 2, 3],
-#     [3, 0, 1, 2],
-#     [2, 3, 0, 1]
-#     ])
-img = np.array([
-    [1, 2, 3, 0, 1, 2, 3],
-    [0, 1, 2, 3, 0, 1, 2],
-    [3, 0, 1, 2, 3, 0, 1],
-    [2, 3, 0, 1, 2, 3, 0],
-    [1, 2, 3, 0, 1, 2, 3],
-    [0, 1, 2, 3, 0, 1, 2],
-    [3, 0, 1, 2, 3, 0, 1],
-    ])
-kernel = np.array([
-    [2, 0, 1],
-    [0, 1, 2],
-    [1, 0, 2]
-    ])
+def pool(img, fw, fh, padding=0, stride=1):
+    # change ndim
+    img = img.reshape(((1, 1)+img.shape)[-3:])
 
-conved = conv(img, kernel, stride=2)
-print(conved)
+    p = padding
+    s = stride
+    batch_num, rh, rw = img.shape
+    nh = int((rh + 2*p - fh) / s) + 1
+    nw = int((rw + 2*p - fw) / s) + 1
+
+    if padding != 0:
+        _h, _w = rh, rw
+        rh, rw = rh+2*rp, rw+2*rp
+        new_img = np.zeros((rh, rw))
+        new_img[p:p+_h, p:p+_w] = img
+        img = new_img
+    
+    ret = np.zeros((batch_num, nh, nw))
+    for bn in range(batch_num):
+        for i in range(nh):
+            for j in range(nw):
+                # import pdb; pdb.set_trace()
+                t = img[bn, s*i:s*i+fh, s*j:s*j+fw]
+                ret[bn, i, j] += np.max(t)
+    
+    return ret
+
+# img = np.array([
+#     [1, 2, 3, 0, 1, 2, 3],
+#     [0, 1, 2, 3, 0, 1, 2],
+#     [3, 0, 1, 2, 3, 0, 1],
+#     [2, 3, 0, 1, 2, 3, 0],
+#     [1, 2, 3, 0, 1, 2, 3],
+#     [0, 1, 2, 3, 0, 1, 2],
+#     [3, 0, 1, 2, 3, 0, 1],
+#     ])
+# kernel = np.array([
+#     [2, 0, 1],
+#     [0, 1, 2],
+#     [1, 0, 2]
+#     ])
+# conved = conv(img, kernel, stride=2)
+# print(conved)
+
+img = np.array([
+    [1, 2, 3, 0],
+    [0, 1, 2, 3],
+    [3, 0, 1, 2],
+    [2, 3, 0, 1]
+    ])
+pooled = pool(img, 3, 3)
+print(pooled)
