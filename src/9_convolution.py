cb53dd094a65a532d49199c8beb6fe30034229aa
piyo56 <piyo56.net@gmail.com>
2017-07-04 19:06:31 +0900

    add bias, padding, stride

--- src/9_convolution.py
+++ src/9_convolution.py
@@ -3,30 +3,48 @@
 import sys
 import numpy as np
 
-def conv(img, kernel):
+def conv(img, kernel, bias = 0, padding = 0, stride = 1):
+    p = padding
+    s = stride
     h, w   = img.shape
-    kh, kw = kernel.shape
+    fh, fw = kernel.shape
+    nh = int((h + 2*p - fh) / s) + 1
+    nw = int((w + 2*p - fw) / s) + 1
 
     if kernel.ndim != 1:
         kernel = kernel.reshape(-1)
 
-    nh = (h-kh) + 1
-    nw = (w-kw) + 1
+    if padding != 0:
+        _h, _w = h, w
+        h, w = h+2*p, w+2*p
+        new_img = np.zeros((h, w))
+        new_img[p:p+_h, p:p+_w] = img
+        img = new_img
 
-    c = np.zeros((nw, nh))
+    c = np.zeros((nh, nw))
     
-    for i in range(nh):
-        for j in range(nw):
-            p = img[i:i+kh, j:j+kw].reshape(-1)
-            c[i, j] = np.dot(p, kernel)
+    for i in range(0, nh):
+        for j in range(0, nw):
+            t = img[s*i:s*i+fh, s*j:s*j+fw].reshape(-1)
+            # import pdb; pdb.set_trace()
+            c[i, j] = np.dot(t, kernel)
 
-    return c
+    return c + bias
 
+# img = np.array([
+#     [1, 2, 3, 0],
+#     [0, 1, 2, 3],
+#     [3, 0, 1, 2],
+#     [2, 3, 0, 1]
+#     ])
 img = np.array([
-    [1, 2, 3, 0],
-    [0, 1, 2, 3],
-    [3, 0, 1, 2],
-    [2, 3, 0, 1]
+    [1, 2, 3, 0, 1, 2, 3],
+    [0, 1, 2, 3, 0, 1, 2],
+    [3, 0, 1, 2, 3, 0, 1],
+    [2, 3, 0, 1, 2, 3, 0],
+    [1, 2, 3, 0, 1, 2, 3],
+    [0, 1, 2, 3, 0, 1, 2],
+    [3, 0, 1, 2, 3, 0, 1],
     ])
 kernel = np.array([
     [2, 0, 1],
@@ -34,5 +52,5 @@ kernel = np.array([
     [1, 0, 2]
     ])
 
-conved = conv(img, kernel)
+conved = conv(img, kernel, stride=2)
 print(conved)
