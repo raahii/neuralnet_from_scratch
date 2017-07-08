cc3a296c982757e43fdc316f4db9d065a3123c8e
piyo56 <piyo56.net@gmail.com>
2017-07-08 13:01:15 +0900

    remove activation_functions, integrate to layes

--- lib/layers.py
+++ lib/layers.py
@@ -7,8 +7,7 @@ from lib.common_functions import *
 from lib.utils import im2col, col2im
 
 class Affine:
-    def __init__(self, input_size, output_size, activation_function,
-                 init_method="gaussian"):
+    def __init__(self, input_size, output_size, init_method="gaussian"):
 
         self.input_size = input_size
         self.output_size = output_size
@@ -18,7 +17,6 @@ class Affine:
         self.dW = None
         self.db = None
         self.x  = None
-        self.activation_functions = np.array([activation_function]).flatten()
 
         self.org_shape = None
 
@@ -37,15 +35,10 @@ class Affine:
         self.x = x.reshape(x.shape[0], -1)
 
         y = np.dot(self.x, self.W) + self.b
-        
-        for af in self.activation_functions:
-            y = af.forward(y)
 
         return y
 
     def backward(self, dy):
-        for af in reversed(self.activation_functions):
-            dy = af.backward(dy)
 
         self.dW = np.dot(self.x.T, dy)
         self.db = np.sum(dy, axis=0)
@@ -63,8 +56,7 @@ class Affine:
         self.b -= lr * self.db
 
 class Conv:
-    def __init__(self, activation_function,
-                 filter_shape, stride=1, padding=0,
+    def __init__(self, filter_shape, stride=1, padding=0, 
                  init_method="gaussian", train_flg = True):
         
         # self.C, self.IH, self.IW = input_shape
@@ -74,8 +66,6 @@ class Conv:
         self.b = np.zeros(self.FN)
         self.S = stride
         self.P = padding
-
-        self.activation_functions = np.array([activation_function]).flatten()
 
         self.dW = None
         self.db = None
@@ -105,16 +95,9 @@ class Conv:
         self.col_x = col_x
         self.col_W = col_W
 
-        # apply avtivation functions
-        for af in self.activation_functions:
-            y = af.forward(y)
-
         return y
 
     def backward(self, dy):
-        for af in reversed(self.activation_functions):
-            dy = af.backward(dy)
-
         FN, C, FH, FW = self.W.shape
         dy = dy.transpose(0, 2, 3, 1).reshape(-1, FN)
 
@@ -129,3 +112,243 @@ class Conv:
     def update(self, lr):
         self.W -= lr * self.dW
         self.b -= lr * self.db
+
+class Sigmoid:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, train_flg=True):
+        self.x = x
+        self.y = sigmoid(x)
+
+        return self.y
+    
+    def backward(self, dy):
+        dx = dy * (1.0 - self.y) * self.y
+        
+        return dx
+
+    def update(self, lr = 0.1):
+        pass
+
+class Relu:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, train_flg=True):
+        self.x = x
+        self.y = relu(self.x)
+
+        return self.y
+
+    def backward(self, dy):
+        dx = dy.copy()
+        dx[ self.x<=0 ] = 0
+
+        return dx
+
+    def update(self, lr = 0.1):
+        pass
+
+class Softmax:
+    def __init__(self):
+        self.x = None
+
+    def forward(self, x, train_flg=True):
+        self.x = x
+        y = softmax(self.x)
+
+        return y
+    
+    def backward(self, dy):
+        batch_size = self.x.shape[0]
+        dx = dy / batch_size
+        
+        return dx
+
+    def update(self, lr = 0.1):
+        pass
+
+class BatchNormalization:
+    def __init__(self, lr=0.1):
+        self.eps        = 1e-7
+        self.batch_size = None
+
+        self.x      = None
+        self.x_norm = None
+        self.x_c    = None
+        self.mu     = None
+        self.var    = None
+
+        self.gamma  = None
+        self.beta   = 0.0
+        self.dbeta  = None
+        self.dgamma = None
+
+    def forward(self, x, train_flg=True):
+        if x.ndim != 2:
+            self.x = x.reshape(x.shape[0], -1)
+        else:
+            self.x = x
+
+        self.batch_size = x.shape[0]
+        self.input_dim = x.shape[1]
+
+        if self.gamma is None:
+            self.gamma = np.ones(self.input_dim, )
+
+        self.mu = np.mean(x, axis = 0)
+        self.std = np.std(x, axis = 0)
+        self.xc = x - self.mu
+
+        self.x_norm = self.xc / (self.std + self.eps)
+        y = self.gamma * self.x_norm + self.beta
+
+        return y
+
+    def backward(self, dy):
+        self.dbeta = np.sum(dy)
+        self.dgamma = np.sum(self.x_norm * dy, axis = 0)
+
+        dx_norm = dy * self.gamma
+
+        dxc = dx_norm / self.std
+        dstd = -np.sum(dx_norm * self.xc / (self.std * self.std), axis=0)
+        dvar = 0.5 * dstd / self.std
+        dxc += 2.0 / self.batch_size * self.xc * dvar
+        dmu = np.sum(dxc, axis=0)
+        dx = dxc - dmu / self.batch_size
+        # dx = dxc * ( 1.0 - 1.0 / self.batch_size)
+
+        return dx
+
+    def update(self, lr = 0.1):
+        self.gamma -= lr * self.dgamma
+        self.beta  -= lr * self.dbeta
+
+class Dropout:
+    def __init__(self, dropout_rate=0.5):
+        self.dropout_rate = dropout_rate
+
+    def forward(self, x, train_flg=True):
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_rate
+            return x * self.mask
+        else:
+            return x
+
+    def backward(self, dy):
+        return dy * self.mask
+
+    def update(self, lr = 0.1):
+        pass
+
+class Pooling:
+    def __init__(self, FH, FW, padding=0, stride=1):
+        self.FH = FH
+        self.FW = FW
+        self.S = stride
+        self.P = padding
+
+    def forward(self, x, train_flg=True):
+        BS, C, IH, IW = x.shape
+        OH = int( (IH+2*self.P-self.FH) / self.S + 1 )
+        OW = int( (IW+2*self.P-self.FW) / self.S + 1 )
+
+        col_x = im2col(x, self.FH, self.FW, self.S, self.P)
+        col_x = col_x.reshape(-1, self.FW*self.FH)
+
+        arg_max = np.argmax(col_x, axis=1)
+        col_y = np.max(col_x, axis = 1)
+        y = col_y.reshape(BS, OH, OW, C).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.arg_max = arg_max
+
+        return y
+
+    def backward(self, dy):
+        dy = dy.transpose(0, 2, 3, 1)
+        
+        pool_size = self.FH * self.FW
+        dmax = np.zeros((dy.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dy.flatten()
+        dmax = dmax.reshape(dy.shape + (pool_size,)) 
+        
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dx = col2im(dcol, self.x.shape, self.FH, self.FW, self.S, self.P)
+        
+        return dx
+
+    def update(self, lr = 0.1):
+        pass
+
+class LRN:
+    """
+    Local Response Normalization
+    """
+    def __init__(self, alpha=1e-4, k=2, beta=0.75, n=5):
+        self.alpha = alpha
+        self.k = k
+        self.beta = beta
+        self.n = n
+        self.r = int(self.n / 2.0)
+        
+        self.x = None
+        self.d2 = None
+        self.d3 = None
+        self.sums = None
+
+    def forward(self, x, train_flg=True):
+        BN, C, IH, IW = x.shape
+        x = x.transpose(1, 0, 2, 3).reshape(C, BN, -1)
+
+        _sums = np.sum(np.power(x, 2), axis=2)
+        sums = np.zeros( (C, BN, ) )
+        for i in range(C):
+            s = max(0, i-self.r)
+            e = min(C, i+self.r)
+            sums[i] = np.sum(_sums[s:e], axis=0)
+
+        sums = sums.reshape((C, BN, 1))
+        d2 = self.k + self.alpha * sums
+        d3 = np.power(d2, -self.beta)
+
+        y = x * d3
+        y = y.reshape(C, BN, IH, IW).transpose(1, 0, 2, 3)
+
+        self.x = x
+        self.d2 = d2
+        self.d3 = d3
+        self.sums = sums
+
+        return y
+
+    def backward(self, dy):
+        BN, C, IH, IW = dy.shape
+        dy = dy.transpose(1, 0, 2, 3).reshape(C, BN, -1)
+
+        dx = dy * self.d3
+
+        dd3 = self.x * dy
+        dd2 = dd3 * -self.beta * np.power(self.d2, -(self.beta+1))
+        
+        # dd1 = np.zeros_like(dd2)
+        # ones = np.ones((BN, IH*IW))
+
+        # TODO: should simplify
+        # for i in range(C):
+        #     s = max(0, i-self.r)
+        #     e = min(C, i+self.r)
+        #     dd1[s:e] += ones
+
+        dx += dd2 * 2.0 * self.alpha * self.sums # dd1 
+
+        dx = dx.reshape(C, BN, IH, IW).transpose(1, 0, 2, 3)
+
+        return dx
+
+    def update(self, lr = 0.1):
+        pass
--- src/11_AlexNet.py
+++ src/11_AlexNet.py
@@ -10,34 +10,38 @@ import pickle
 from dataset.mnist import load_mnist
 from lib.my_neural_net import MyNeuralNet
 from lib.common_functions import cross_entropy_error
-from lib.layers import Affine, Conv
-from lib.activation_functions import *
+from lib.layers import *
 
 # データセットのロード
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=False, one_hot_label=True)
 
 # ネットワークを定義
-network = MyNeuralNet( cost_function = cross_entropy_error)
+network = MyNeuralNet(cost_function = cross_entropy_error)
 
-conv1 = Conv( activation_function = [Relu(), LRN(), Pooling(3, 3), Dropout(0.5)], filter_shape = (96,  1, 3, 3) )
-# conv1 = Conv( activation_function = [Relu()], filter_shape = (96, 1, 4, 4))
-# conv2 = Conv( activation_function = [Relu(), LRN(), Pooling(3, 3), Dropout(0.5)], filter_shape = (256,  96, 3, 3) )
-# conv3 = Conv( activation_function = [Relu(), LRN(), Pooling(3, 3), Dropout(0.6)], filter_shape = (384, 256, 3, 3) )
-# conv4 = Conv( activation_function = [Relu(), LRN(), Pooling(3, 3), Dropout(0.7)], filter_shape = (384, 384, 3, 3) )
-# conv5 = Conv( activation_function = [Relu(), LRN(), Pooling(3, 3), Dropout(0.8)], filter_shape = (256, 384, 3, 3) )
+conv1 = Conv( filter_shape = (96,  1, 3, 3) )
+# conv1 = Conv( filter_shape = (96, 1, 4, 4) )
+# conv2 = Conv( filter_shape = (256,  96, 3, 3) )
+# conv3 = Conv( filter_shape = (384, 256, 3, 3) )
+# conv4 = Conv( filter_shape = (384, 384, 3, 3) )
+# conv5 = Conv( filter_shape = (256, 384, 3, 3) )
 # out_size= 9
-# affine1 = Affine( 256*out_size**2, 256*out_size**2, Relu(), "he" )
-# affine2 = Affine( 256*out_size**2, 10, Softmax(), "he" )
+# affine1 = Affine( 256*out_size**2, 256*out_size**2, "he" )
+# affine2 = Affine( 256*out_size**2, 10, "he" )
 out_size= 24
-affine2 = Affine( 96*out_size**2, 10, Softmax(), "he" )
+affine2 = Affine( 96*out_size**2, 10, "he" )
 
 network.add_layer(conv1)
+network.add_layer(Relu())
+network.add_layer(LRN())
+network.add_layer(Pooling(3, 3))
+network.add_layer(Dropout(0.5))
 # network.add_layer(conv2)
 # network.add_layer(conv3)
 # network.add_layer(conv4)
 # network.add_layer(conv5)
 # network.add_layer(affine1)
 network.add_layer(affine2)
+network.add_layer(Softmax())
 
 # 学習
 train_size = 10000
