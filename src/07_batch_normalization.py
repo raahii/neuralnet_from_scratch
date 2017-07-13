acf8cb9bcae45011278e74741b9bee6fef10d430
piyo56 <piyo56.net@gmail.com>
2017-07-13 22:13:37 +0900

    fix 07_batch_normalization.py

--- src/07_batch_normalization.py
+++ src/07_batch_normalization.py
@@ -4,10 +4,10 @@ sys.path.append(os.pardir)
 from dataset.mnist import load_mnist
 from lib.common_functions import cross_entropy_error
 from lib.my_neural_net import MyNeuralNet
-from lib.layers import Affine
-from lib.activation_functions import Sigmoid, Softmax, Relu, BatchNormalization
+from lib.layers import *
 
 import numpy as np
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 
@@ -15,28 +15,26 @@ plt.style.use("ggplot")
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=True)
 
 # 28x28の画像を入力し、0-9の文字のいずれかを知りたい
-networks = {
-            "normal": MyNeuralNet(cross_entropy_error),
-            "batch_normalization": MyNeuralNet(cross_entropy_error),
-           }
+network = MyNeuralNet(cross_entropy_error)
 
 # レイヤを追加
-LAYER_NUM = 6
-network = networks["normal"]
-network.add_layer(Affine(28*28, 100, Sigmoid(), "xavier"))
-network.add_layer(Affine(100,   100, Sigmoid(), "xavier"))
-network.add_layer(Affine(100,   100, Sigmoid(), "xavier"))
-network.add_layer(Affine(100,   100, Sigmoid(), "xavier"))
-network.add_layer(Affine(100,   100, Sigmoid(), "xavier"))
-network.add_layer(Affine(100,   10,  Softmax(), "xavier"))
-
-network = networks["batch_normalization"]
-network.add_layer(Affine(28*28, 100, [BatchNormalization(), Sigmoid()], "xavier"))
-network.add_layer(Affine(100,   100, [BatchNormalization(), Sigmoid()], "xavier"))
-network.add_layer(Affine(100,   100, [BatchNormalization(), Sigmoid()], "xavier"))
-network.add_layer(Affine(100,   100, [BatchNormalization(), Sigmoid()], "xavier"))
-network.add_layer(Affine(100,   100, [BatchNormalization(), Sigmoid()], "xavier"))
-network.add_layer(Affine(100,   10 , Softmax(),                         "xavier"))
+network.add_layer(Affine(28*28, 50, "he"))
+network.add_layer(BatchNormalization())
+network.add_layer(Relu())
+network.add_layer(Affine(50, 100, "he"))
+network.add_layer(BatchNormalization())
+network.add_layer(Relu())
+network.add_layer(Affine(100, 100, "he"))
+network.add_layer(BatchNormalization())
+network.add_layer(Relu())
+network.add_layer(Affine(100, 100, "he"))
+network.add_layer(BatchNormalization())
+network.add_layer(Relu())
+network.add_layer(Affine(100, 100, "he"))
+network.add_layer(BatchNormalization())
+network.add_layer(Relu())
+network.add_layer(Affine(100, 10, "he"))
+network.add_layer(Softmax())
 
 # 学習
 train_size = x_train.shape[0]
@@ -44,15 +42,15 @@ batch_size = 100
 
 iter_per_epoch = max(int(train_size / batch_size), 1)
 
-iters_num = iter_per_epoch * 50
+epoch_num = 20
+iters_num = iter_per_epoch * epoch_num
 learning_rate = 0.1
 
-plt.figure(figsize=(12,8))
 loss_list = []
 train_acc_list = []
 test_acc_list = []
 
-for i in range(iters_num):
+for i in tqdm(range(iters_num)):
     batch_mask = np.random.choice(train_size, batch_size)
     
     x = x_train[batch_mask]
@@ -61,35 +59,50 @@ for i in range(iters_num):
     y = network.forward(x)
     network.backward(y, t)
 
-    for network in networks.values():
-        y = network.forward(x)
-        network.backward(y, t)
+    for layer in network.layers:
+        layer.update(learning_rate)
 
-        for layer in network.layers:
-            layer.W -= learning_rate * layer.dW
-            layer.b -= learning_rate * layer.db
-
-    if i % iter_per_epoch == 0:
-        print("--- epoch {} ---".format(int(i/iter_per_epoch)))
+    if i != 0 and i % iter_per_epoch == 0:
+        loss_list.append(network.loss(y, t))
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
 
         plt.clf()
-        rows_num = len(networks.keys())
-        columns_num = LAYER_NUM-1
+        plt.subplot(1, 2, 1)
+        x = np.array(range(1, len(loss_list)+1))
+        plt.plot(x, loss_list)
+        plt.xlabel("epoch")
+        plt.ylabel("loss")
+        plt.xlim(1, epoch_num+1)
 
-        for row, n in enumerate(networks.items()):
-            title, network = n
-
-            train_acc = network.accuracy(x_train, t_train)
-            test_acc = network.accuracy(x_test, t_test)
-            print(title, ":", train_acc, test_acc)
-
-            for column, layer in enumerate(network.layers[0:-1]):
-                plt.subplot(rows_num, columns_num, columns_num*(row) + column+1)
-                # plt.suptitle(title)
-                plt.title("{}-layer".format(column+1))
-
-                colors = ["r", "g", "b"]
-                plt.hist(layer.activation_functions[-1].y.flatten(), bins=30, color=colors[row])
+        plt.subplot(1, 2, 2)
+        plt.plot(x, train_acc_list, "r", label="train_acc")
+        plt.plot(x, test_acc_list, "b", label="test_acc")
+        plt.xlabel("epoch")
+        plt.ylabel("acc")
+        plt.xlim(1, epoch_num+1)
+        plt.legend()
 
         plt.draw()
         plt.pause(0.05)
+
+plt.clf()
+plt.subplot(1, 2, 1)
+x = np.array(range(1, len(loss_list)+1))
+plt.plot(x, loss_list)
+plt.xlabel("epoch")
+plt.ylabel("loss")
+plt.xlim(1, epoch_num+1)
+
+plt.subplot(1, 2, 2)
+plt.plot(x, train_acc_list, "r", label="train_acc")
+plt.plot(x, test_acc_list, "b", label="test_acc")
+plt.xlabel("epoch")
+plt.ylabel("acc")
+plt.xlim(1, epoch_num+1)
+plt.legend()
+
+plt.savefig("../data/batch_normalization.png")
+print("train acc: {}, test acc: {}".format(train_acc_list[-1], test_acc_list[-1]))
