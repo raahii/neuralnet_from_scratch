3b5cc649425b39a55e17e81c7b0aa08b8d031cc8
piyo56 <piyo56.net@gmail.com>
2017-07-18 17:36:47 +0900

    fix alexnet.py

--- src/11_AlexNet.py
+++ src/11_AlexNet.py
@@ -18,12 +18,11 @@ from lib.trainer import Trainer
 network = MyNeuralNet(cost_function = cross_entropy_error)
 
 conv1 = Conv( filter_shape = (96,  1, 3, 3) )
-conv1 = Conv( filter_shape = (96, 1, 4, 4) )
 conv2 = Conv( filter_shape = (256,  96, 3, 3) )
 conv3 = Conv( filter_shape = (384, 256, 3, 3) )
 conv4 = Conv( filter_shape = (384, 384, 3, 3) )
 conv5 = Conv( filter_shape = (256, 384, 3, 3) )
-out_size= 9
+out_size= 8
 affine1 = Affine( 256*out_size**2, 256*out_size**2, "he" )
 affine2 = Affine( 256*out_size**2, 10, "he" )
 
@@ -38,6 +37,10 @@ network.add_layer(LRN())
 network.add_layer(Pooling(3, 3))
 
 network.add_layer(conv3)
+network.add_layer(Relu())
+network.add_layer(LRN())
+network.add_layer(Pooling(3, 3))
+
 network.add_layer(conv4)
 network.add_layer(Relu())
 network.add_layer(LRN())
